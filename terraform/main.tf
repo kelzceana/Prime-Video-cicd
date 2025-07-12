b61c0b9ad377a64cb82046be5909eea95f387ce3
Kelechi Obikaonu <71353368+kelzceana@users.noreply.github.com>
2025-07-12 16:25:44 -0400

    provision ec2 with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,7 @@ resource "aws_vpc" "myapp-vpc" {
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id     = aws_vpc.myapp-vpc.id
   cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
 
   tags = {
     Name = "${var.env_prefix}-subnet"
